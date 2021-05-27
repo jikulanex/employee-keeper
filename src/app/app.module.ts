54d35059dd189f91b49f9b31cc718fa4327cc42c
jikulanex <siegfred.balona@lanexus.com>
2021-05-27 19:29:30 +0800

    Setup lazy loading both for employee form and skills form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,38 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { EmployeesModule } from './employees/employees.module';
+import { SkillsModule } from './skills/skills.module';
+
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+// 'Angular Material' components
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    EmployeesModule, // Load 'Employee table' module.
+    SkillsModule, // Load 'Skill table' module.
+    // Important note:
+    // The 'App module' must be put below other existing modules to avoid
+    // conflicts during the path resolution process.
+    AppRoutingModule, // App module
+    // Important note:
+    // This is the start of loading 'Angular Material' components.
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
