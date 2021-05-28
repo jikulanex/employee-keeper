1d887842e7a65d0ae76af3dcdd6ef38338771d31
jikulanex <siegfred.balona@lanexus.com>
2021-05-28 18:16:12 +0800

    Refactor employee (table) module

--- src/app/employees/employee-table/employee-table.component.ts
+++ src/app/employees/employee-table/employee-table.component.ts
@@ -6,7 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-table.component.css'],
 })
 export class EmployeeTableComponent implements OnInit {
+  today = new Date().getFullYear();
+
+  employeeData = [
+    {
+      id: 0,
+      firstName: 'Sieg',
+      lastName: 'Balona',
+      birthDate: '1988-07-31',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+    },
+  ];
+
+  displayedColumns = [
+    'id',
+    'firstName',
+    'lastName',
+    'birthDate',
+    'age',
+    'skills',
+    'controls',
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  getAge(birthDate: string) {
+    return this.today - new Date(birthDate).getFullYear();
+  }
 }
--- src/app/employees/employees.module.ts
+++ src/app/employees/employees.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { EmployeesRoutingModule } from './employees-routing.module';
 import { EmployeeTableComponent } from './employee-table/employee-table.component';
+
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+// Load the shared module to get access to the 'Table' component.
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [EmployeeTableComponent],
-  imports: [CommonModule, EmployeesRoutingModule, MatTableModule],
+  imports: [
+    CommonModule,
+    EmployeesRoutingModule,
+    SharedModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class EmployeesModule {}
