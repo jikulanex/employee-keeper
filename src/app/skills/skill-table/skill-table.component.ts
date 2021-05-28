7e79e20cfa579961ef5cd0088ccbe48178da06db
jikulanex <siegfred.balona@lanexus.com>
2021-05-28 18:17:32 +0800

    Refactor skills (table) module

--- src/app/skills/skill-table/skill-table.component.ts
+++ src/app/skills/skill-table/skill-table.component.ts
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skill-table.component.css'],
 })
 export class SkillTableComponent implements OnInit {
+  employeeData = [{ id: 0, skill: 'JavaScript' }];
+
+  displayedColumns = ['id', 'skill', 'controls'];
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/skills/skills.module.ts
+++ src/app/skills/skills.module.ts
@@ -3,10 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { SkillsRoutingModule } from './skills-routing.module';
 import { SkillTableComponent } from './skill-table/skill-table.component';
+
 import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+// Load the shared module to get access to the 'Table' component.
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SkillTableComponent],
-  imports: [CommonModule, SkillsRoutingModule, MatTableModule],
+  imports: [
+    CommonModule,
+    SkillsRoutingModule,
+    SharedModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
 })
 export class SkillsModule {}
