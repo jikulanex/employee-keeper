df1b9f6e1e07544b22698f34e04a6ddb2a42bec0
jikulanex <siegfred.balona@lanexus.com>
2021-05-30 16:29:53 +0800

    Update shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FormComponent } from './form/form.component';
-
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [FormComponent],
-  imports: [
-    CommonModule,
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
-    MatTableModule,
-  ],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [FormComponent],
 })
 export class SharedModule {}
