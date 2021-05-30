114f0420249efdf50025d15ebeda1abb34a75bcb
jikulanex <siegfred.balona@lanexus.com>
2021-05-30 16:29:16 +0800

    Refactor skills (table) module

--- src/app/skills/skills.module.ts
+++ src/app/skills/skills.module.ts
@@ -4,22 +4,8 @@ import { CommonModule } from '@angular/common';
 import { SkillsRoutingModule } from './skills-routing.module';
 import { SkillTableComponent } from './skill-table/skill-table.component';
 
-import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-
-// Load the shared module to get access to the 'Table' component.
-import { SharedModule } from '../shared/shared.module';
-
 @NgModule({
   declarations: [SkillTableComponent],
-  imports: [
-    CommonModule,
-    SkillsRoutingModule,
-    SharedModule,
-    MatTableModule,
-    MatButtonModule,
-    MatIconModule,
-  ],
+  imports: [CommonModule, SkillsRoutingModule],
 })
 export class SkillsModule {}
