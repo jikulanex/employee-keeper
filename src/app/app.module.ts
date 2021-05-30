e7904b54d80652d3b1c29dba038188c9d99206b0
jikulanex <siegfred.balona@lanexus.com>
2021-05-30 16:18:28 +0800

    Refactor app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,6 @@ import { SkillsModule } from './skills/skills.module';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-// 'Angular Material' components
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
-
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [
@@ -27,13 +20,6 @@ import { MatMenuModule } from '@angular/material/menu';
     // The 'App module' must be put below other existing modules to avoid
     // conflicts during the path resolution process.
     AppRoutingModule, // App module
-    // Important note:
-    // This is the start of loading 'Angular Material' components.
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
