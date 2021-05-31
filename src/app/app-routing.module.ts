3174628af961088e164fb37e90e95a1ac5e17bfd
Siegfred Balona <siegfred.balona@lanexus.com>
2021-05-31 14:54:54 +0800

    Update list of routes in app module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,23 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
-// Lazy load 'Employee form' module by implementing 'Dynamic import' syntax.
-const loadEmployeeFormModule = () => {
-  return import('./employee-form/employee-form.module').then((module) => {
-    return module.EmployeeFormModule;
-  });
-};
-
-// Lazy load 'Skills form' module by implementing 'Dynamic import' syntax.
-const loadSkillsFormModule = () => {
-  return import('./skills-form/skills-form.module').then((module) => {
-    return module.SkillsFormModule;
-  });
-};
-
 const routes: Routes = [
-  { path: 'employee-form', loadChildren: loadEmployeeFormModule },
-  { path: 'skills-form', loadChildren: loadSkillsFormModule },
   { path: '', component: HomeComponent },
   { path: '**', component: NotFoundComponent },
 ];
