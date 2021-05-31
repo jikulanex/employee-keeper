02249bc4c91b61e7864e07d43cdf78ade32c29bb
Siegfred Balona <siegfred.balona@lanexus.com>
2021-05-31 10:55:23 +0800

    Update initial skills data in skills service

--- src/app/services/skill-service.service.ts
+++ src/app/services/skill-service.service.ts
@@ -9,7 +9,15 @@ interface Skill {
   providedIn: 'root',
 })
 export class SkillService {
-  skills: Array<Skill> = [];
+  skills: Array<Skill> = [
+    { id: 1, name: 'HTML' },
+    { id: 2, name: 'CSS' },
+    { id: 3, name: 'JavaScript' },
+    { id: 4, name: 'Node.js' },
+    { id: 5, name: 'Angular' },
+    { id: 6, name: 'Vue.js' },
+    { id: 7, name: 'React.js' },
+  ];
 
   constructor() {}
 
