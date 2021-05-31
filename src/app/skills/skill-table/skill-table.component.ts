c577d29eafda94c12d2cf672b85cf3fa4b6d803d
Siegfred Balona <siegfred.balona@lanexus.com>
2021-05-31 18:09:05 +0800

    Update skill table component

--- src/app/skills/skill-table/skill-table.component.ts
+++ src/app/skills/skill-table/skill-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { SkillService } from '../../services/skill-service.service';
 import { LocalStorageService } from '../../services/local-storage.service';
 
@@ -13,6 +14,7 @@ interface Skill {
   styleUrls: ['./skill-table.component.css'],
 })
 export class SkillTableComponent implements OnInit {
+  formIsSubmitted = false;
   skills: Array<Skill> = [];
 
   thHead = Array(21)
@@ -24,11 +26,13 @@ export class SkillTableComponent implements OnInit {
     .map((x, i) => i);
 
   constructor(
+    private router: Router,
     private skillService: SkillService,
     private localStorageService: LocalStorageService
   ) {}
 
   ngOnInit(): void {
+    // Fetch existing skill data.
     const data: any = this.localStorageService.getItem('skills');
 
     if (data?.length) {
@@ -37,5 +41,37 @@ export class SkillTableComponent implements OnInit {
     }
 
     this.skills = this.skillService.getSkills();
+  }
+
+  editSkill(skill: Skill) {
+    console.log('Skill data', skill);
+    this.router.navigate([`/update-skill-form/${skill.id}`]);
+  }
+
+  deleteSkill(skill: Skill) {
+    // Filter out data based on the given skill id.
+    const updatedSkillData = this.skills.filter(
+      (data) => data.id !== Number(skill.id)
+    );
+    console.log('Updated skill data', updatedSkillData);
+
+    // Update the skills array data.
+    this.skillService.updateSkills(updatedSkillData);
+
+    // Store the skills array data to local storage.
+    this.localStorageService.setItem('skills', this.skillService.getSkills());
+
+    // Reload page
+    window.location.reload();
+
+    this.displayNotification();
+  }
+
+  displayNotification() {
+    this.formIsSubmitted = true;
+
+    setTimeout(() => {
+      this.formIsSubmitted = false;
+    }, 2000);
   }
 }
