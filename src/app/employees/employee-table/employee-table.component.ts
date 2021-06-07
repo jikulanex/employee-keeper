2b12cb9130a96444b193ee24fb24a6ae6aeeaa89
Siegfred Balona <siegfred.balona@lanexus.com>
2021-06-07 14:40:51 +0800

    Refactor employee table component

--- src/app/employees/employee-table/employee-table.component.ts
+++ src/app/employees/employee-table/employee-table.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { EmployeeService } from '../../services/employee-service.service';
-import { LocalStorageService } from '../../services/local-storage.service';
+import { EmployeeService } from 'src/app/services/employee-service.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 interface Skill {
-  id: number;
+  _id?: string;
   name: string;
+  __v?: number;
 }
 
 interface Employee {
-  id: number;
+  _id: string;
   firstName: string;
   lastName: string;
   birthDate: string;
   skills: Array<Skill>;
+  __v?: number;
 }
 
 @Component({
@@ -47,17 +49,29 @@ export class EmployeeTableComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // Fetch existing skill data.
-    const localStorageData: any = this.localStorageService.getItem('employees');
+    // Fetch existing employee data.
+    const localStorageEmployeeData: string | null =
+      this.localStorageService.getItem('employees');
 
-    if (localStorageData?.length) {
-      this.employees = JSON.parse(localStorageData);
-      console.log('Existing employee data', this.employees);
-      return;
-    }
+    this.employeeService.getEmployees().subscribe((response: any) => {
+      if (localStorageEmployeeData?.length) {
+        // When the data length from local storage is smaller than the data length from the database.
+        if (localStorageEmployeeData?.length < response.data?.length) {
+          console.info('Http get response', response);
 
-    this.employees = this.employeeService.getEmployees();
-    console.log('Existing employee data', this.employees);
+          // Assign the data from database.
+          this.employees = response.data;
+          return;
+        }
+        // Assign the data from local storage.
+        this.employees = JSON.parse(localStorageEmployeeData);
+        return;
+      }
+
+      console.info('Http get response', response);
+      // Assign the data from database since at this point local storage returns null.
+      this.employees = response.data;
+    });
   }
 
   // Displays the employee details.
@@ -78,7 +92,7 @@ export class EmployeeTableComponent implements OnInit {
   // Displays on 3 skills in the table and hides the rest.
   displaySkills(skills: Array<Skill>) {
     return skills
-      .map((skill) => skill.name)
+      .map((skill) => skill)
       .slice(0, 3)
       .join(', ');
   }
@@ -86,30 +100,25 @@ export class EmployeeTableComponent implements OnInit {
   // Navigate to the update employee form.
   editEmployee(employee: Employee) {
     console.log('Employee data', employee);
-    this.router.navigate([`/update-employee-form/${employee.id}`]);
+    this.router.navigate([`/update-employee-form/${employee._id}`]);
   }
 
   // Deletes one employee.
   deleteEmployee(employee: Employee) {
-    // Filter out data based on the given skill id.
-    const updatedEmployeeData = this.employees.filter(
-      (data) => data.id !== Number(employee.id)
-    );
-    console.log('Updated employee data', updatedEmployeeData);
-
     // Update the skills array data.
-    this.employeeService.updateEmployees(updatedEmployeeData);
+    this.employeeService.deleteEmployee(employee._id).subscribe((response) => {
+      console.log('Http delete response', response);
 
-    // Store the skills array data to local storage.
-    this.localStorageService.setItem(
-      'employees',
-      this.employeeService.getEmployees()
-    );
+      this.employeeService.getEmployees().subscribe((response: any) => {
+        // Store the skills array data to local storage.
+        this.localStorageService.setItem('employees', response.data);
 
-    // Reload page
-    window.location.reload();
+        // Reload page
+        window.location.reload();
 
-    this.displayNotification();
+        this.displayNotification();
+      });
+    });
   }
 
   // Displays the notification component.
